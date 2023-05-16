2659defe5a06d1d930d735cee77cd3fe3448c219
ZUBAIDH <130825715+ZUBAIDH@users.noreply.github.com>
2023-05-15 23:27:07 -0700

    Update ChatBotDemo.java

--- ChatBotDemo.java
+++ ChatBotDemo.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
